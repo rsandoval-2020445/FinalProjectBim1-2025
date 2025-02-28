cf8899b2338157ea0b0336968a476a1a0b3c8e87
rsandoval-2020445 <rsandoval-2020445@kinal.edu.gt>
2025-02-27 18:57:12 -0600

    Definidas las rutas para productos más vendidos y fuera de stock en product.routes.js

--- src/product/product.routes.js
+++ src/product/product.routes.js
@@ -1,22 +1,29 @@
-import { Router } from "express"
-import { 
-  createProduct, 
-  getAllProducts, 
-  getProductById, 
-  updateProduct, 
-  deleteProduct 
-} from "./product.controller.js"
-import { validateJwt, isAdmin } from "../../middlewares/validate.jwt.js"
+import { Router } from 'express'
+import {
+  createProduct,
+  getAllProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+  getOutOfStockProducts,   
+  getTopSellingProduct     
+} from './product.controller.js'
+import { validateJwt, isAdmin } from '../../middlewares/validate.jwt.js'
 
 const router = Router()
 
+//productos más vendidos y productos fuera de stock
+router.get('/top-selling', getTopSellingProduct)  
+router.get('/outofstock', getOutOfStockProducts) 
+
 // Rutas públicas (no necesitan autenticación)
-router.get("/", getAllProducts)    // Obtener todos los productos
-router.get("/:id", getProductById) // Obtener un producto por ID
+router.get('/', getAllProducts)  
+router.get('/:id', getProductById)  
 
 // Rutas protegidas (solo ADMIN)
-router.post("/create", validateJwt, isAdmin, createProduct) // Crear producto
-router.put("/:id", validateJwt, isAdmin, updateProduct)     // Actualizar producto
-router.delete("/:id", validateJwt, isAdmin, deleteProduct)  // Eliminar producto
+router.post('/create', validateJwt, isAdmin, createProduct)  
+router.put('/:id', validateJwt, isAdmin, updateProduct) 
+router.delete('/:id', validateJwt, isAdmin, deleteProduct) 
+
 
 export default router
